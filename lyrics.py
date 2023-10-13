06700d23f30a363a65c549e31d8e358166bba344
Adria Labay <adria.labay@gmail.com>
2023-10-13 14:22:58 +0200

    update to new html classes

--- lyrics.py
+++ lyrics.py
@@ -1,5 +1,6 @@
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SpanishStemmer
+from nltk.stem import WordNetLemmatizer
 from nltk.stem.arlstem import ARLSTem
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -15,11 +16,14 @@ CODE_PLATERO = 'platero-y-tu' # 16157
 CODE_FITO = 'fito-fitipaldis' # 5850
 CODE_EXTREMO = 'extremoduro' # 5663
 CODE_ROBE = 'robe-iniesta' # 36260
+CODE_TAYLOR = 'taylor-swift'
 
-STOPWORDS = stopwords.words('spanish')
+STOPWORDS = stopwords.words('english')
+
+print(STOPWORDS)
 
 def artist_url(artist_code):
-	return BASE_URL + f'/{artist_code}/mais-tocadas.html'
+	return BASE_URL + f'/{artist_code}/mais_acessadas.html'
 	# return BASE_URL + f'letras.asp?letras={artist_code}&orden=alf'
 
 def find_artist_songs(artist_code):
@@ -27,20 +31,20 @@ def find_artist_songs(artist_code):
 	# metras.com -> cnt-list-songs
 	with urlopen(artist_url(artist_code)) as web:
 		html = BeautifulSoup(web, 'html.parser')
-		songs = html.find('ul', 'cnt-list-songs')
-		for link in songs.find_all('a', 'song-name'):
+		songs = html.find('ul', 'songList-table-content')
+		for link in songs.find_all('a', 'songList-table-songName'):
 			yield (BASE_URL + link['href'])
 			time.sleep(0.5)
 
 def process_lyrics(text, 
 	remove_duplicates=True,
 	stopwords=None,
-	stem=None):
+	lemm=None):
 	words = text.split(' ')
 	if stopwords:
 		words = filter(lambda w: w not in stopwords, words)
-	if stem:
-		words = map(stem.stem, words)
+	if lemm:
+		words = map(lemm.lemmatize, words)
 	if remove_duplicates:
 		words = list(set(words))
 	return ' '.join(words)
@@ -50,7 +54,7 @@ def lyrics(url):
 	with urlopen(url) as web:
 		html = BeautifulSoup(web, 'html.parser')
 		
-	lyrics = html.find('div', 'cnt-letra')
+	lyrics = html.find('div', 'lyric-original')
 	text = ' '.join([' '.join(verse.strings) for verse in lyrics.find_all('p')])
 		
 	# normalise lyrics
@@ -68,7 +72,7 @@ def generate_wordcloud(artist_code):
 	stem = ARLSTem()
 	with open(artist_file, 'r') as inf:
 		all_songs = inf.read()
-	all_songs = ' '.join([process_lyrics(song, stem=stem) for song in all_songs.split('\n')])
+	all_songs = ' '.join([process_lyrics(song, lemm=WordNetLemmatizer(), stopwords=STOPWORDS) for song in all_songs.split('\n')])
 
 	wc = WordCloud(width=800, height=600, stopwords=STOPWORDS).generate(all_songs)
 	wc.to_file(f'{artist_code}.png')
@@ -79,4 +83,4 @@ def generate_wordcloud(artist_code):
 # generate_wordcloud('estopa')
 # generate_wordcloud('marea')
 # generate_wordcloud('mago-de-oz-musicas')
-generate_wordcloud(CODE_EXTREMO)
+generate_wordcloud(CODE_TAYLOR)
